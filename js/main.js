f5aec07e8be5cc90f324c0ecfc5cf21591c97124
Abror Anvarov <46844042+aanvarov@users.noreply.github.com>
2020-07-11 19:38:52 +0500

    js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
-// test
+$(document).ready(function () {
+    // popup function
+    function popupClose() {
+
+        $('.popup').fadeOut().delay(400);
+        $('.overlay').fadeOut().delay(400);
+        $('body').removeClass('overflow');
+
+    };
+    
+    $('.closeBtn, .overlay').click(function () {
+        popupClose()
+    });
+    
+    function popup() {
+
+        $('.popup').slideDown(300).fadeIn().delay(600);
+        $('.overlay').slideDown(300).fadeIn().delay(600);
+        $('body').addClass('overflow');
+
+
+        document.onkeydown = function (e) {
+            if (e.keyCode == 27) {
+                popupClose();
+                return false;
+            }
+        };
+
+    };
+
+    $('.popupReq').on('click', (e) => {
+        e.preventDefault();
+        popup();
+    });
+
+
+});
