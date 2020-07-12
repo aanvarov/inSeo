bb55dd32a7b213e7a24bd4f25dcdfff15fd118ee
Abror Anvarov <46844042+aanvarov@users.noreply.github.com>
2020-07-12 18:25:23 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,11 +7,11 @@ $(document).ready(function () {
         $('body').removeClass('overflow');
 
     };
-    
+
     $('.closeBtn, .overlay').click(function () {
         popupClose()
     });
-    
+
     function popup() {
 
         $('.popup').slideDown(300).fadeIn().delay(600);
@@ -32,6 +32,43 @@ $(document).ready(function () {
         e.preventDefault();
         popup();
     });
+
+
+    window.addEventListener('scroll', () => {
+
+        if ($(window).width() < 768) {
+            if ($(window).scrollTop() > 40) {
+                $('.navigation').addClass('fixed');
+                $('.hero').addClass('marginTop');
+            } else {
+                $('.hero').removeClass('marginTop');
+                $('.navigation').removeClass('fixed');
+            }
+        }
+
+        if ($(window).scrollTop() > 1000) {
+            $('#back-to-top').fadeIn(300).css('display', 'block');
+        } else {
+            $('#back-to-top').fadeOut(300);
+        }
+    });
+
+    $('#back-to-top').on('click', (e) => {
+        e.preventDefault();
+        $("html, body").animate({ scrollTop: 0 });
+        return false;
+    });
+
+    // if ($(window).width() < 769) {
+    //     $('.priceList').slick({
+    //         dots: true,
+    //         speed: 300,
+    //         slidesToShow: 1,
+    //         arrows: true,
+    //         prevArrow: "<img class='slick-prev' src='img/angleLeft.svg'>",
+    //         nextArrow: "<img class='slick-next' src='img/angleRight.svg'>"
+    //     });
+    // }
 
 
 });
